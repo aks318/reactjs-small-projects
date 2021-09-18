7a53b29f09991963a592a0b1940477d6f330fb52
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2021-09-18 22:41:45 +0530

    useMemo Practice

--- src/component/prac4/AgeIncrement.js
+++ src/component/prac4/AgeIncrement.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 
 const AgeIncrement = () => {
     const [age , setAge] = useState(0)
+    const [name , setName] = useState('Aakash')
 
     const incrementAge = () => {
         setAge(age + 1)
     }
+
+    const usememoFunc = useMemo(() => {
+        let i = 0
+        while(i < 10000*100000) i++
+        return age % 2 ===0 ? 'Even' : 'Odd'
+
+    } , [name])
  
     return (
         <div>
             <button onClick = {incrementAge}>Increment Age</button>
             <h2>{age}</h2>
+            <h2>{usememoFunc}</h2>
         </div>
     )
 }
