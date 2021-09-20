14ae9fb7f117bfa05eefeb600edede5ac275e7f4
Aakash Vishwakarma <akashviahwakarma98@gmail.com>
2021-09-20 19:26:23 +0530

    boxSearch input testing done

--- src/component/prac5/boxSearch.test.js
+++ src/component/prac5/boxSearch.test.js
@@ -2,5 +2,7 @@ import { render , screen  } from "@testing-library/react";
 import BoxSearch from "./BoxSearch";
 
 test('Initial rendering', () => {
-    
+    render(<BoxSearch />)
+    const search = screen.getByRole('textbox')
+    expect(search).toHaveAttribute('placeholder')
 })
